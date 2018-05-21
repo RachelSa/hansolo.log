ea4cc6bb055c9b82213e95fe16b4218bd9567894
github username <rachel.salois@gmail.com>
2018-05-21 17:51:34 -0400

    created log formatting

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-exports.solo = function(msg){
-  console.log(msg)
+exports.solo = (msg) => {
+  printHeader()
+  printMsg(msg)
+}
+
+exports.confident = () => {
+  printHeader()
+  printMsg("You know, sometimes I amaze even myself.")
+}
+
+function printHeader(){
+  console.log(" .     *          .     *            .      ")
+  console.log("          .  *                *  .       .  ")
+  console.log("   .    === MILLENNIUM FALCON ===       *   ")
+  console.log(" .   *   .     starship log     *  .        ")
+  console.log("  *            .            .         *     \n")
+}
+
+function printMsg(msg){
+  let ts = new Date()
+  let dir = process.cwd()
+  console.log(`MESSAGE:\n ${msg}\n`)
+  console.log(dir)
+  console.log(ts)
+  console.log(" --------------------------------- ")
 }
